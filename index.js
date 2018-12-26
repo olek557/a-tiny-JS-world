239540201b52f91950882cab59fcca7f3c884b36
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2018-12-26 23:59:57 +0200

    Populate world

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-
+function Inhabitant(species, name, legs, hands, gender, phrase) {
+   return {
+      species: species,
+      name: name,
+      legs: legs,
+      hands: hands,
+      gender: gender,
+      phrase: phrase,
+      // friends: arguments.slice(6),
+      getInfo: function() {
+         return [species, name, legs, hands, gender, phrase].join("; ");
+      }
+   }
+}
+const dog = Inhabitant('dog', 'Barsik', 4, 0, 'male', 'Woof woof', 'Alex', 'Bobby'),
+      man = Inhabitant('man', 'Max', 2, 2, 'male', 'Hello world!!'),
+      woman = Inhabitant('woman', 'Merry', 2, 2, 'female', 'Hello all'),
+      cat = Inhabitant('cat', 'Tom', 4, 0, 'male', 'Meow'),
+      catWoman = Inhabitant('cat-woman', 'Kitty', 2, 2, 'female', cat.phrase);
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+print(dog.getInfo());
+print(man.getInfo());
+print(woman.getInfo());
+print(cat.getInfo());
+print(catWoman.getInfo());
