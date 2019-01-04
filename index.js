94f46b509d9a2f888f437bc56e56f207caef38cc
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2019-01-04 15:53:46 +0200

    Rewrite code with OOP

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 // ======== OBJECTS DEFINITIONS ========
-function Inhabitant(species, name, legs, hands, gender, phrase) {
-   return {
-      species: species,
-      name: name,
-      legs: legs,
-      hands: hands,
-      gender: gender,
-      phrase: phrase,
-      // friends: arguments.slice(6),
-      getInfo: function() {
-         return [species, name, legs, hands, gender, phrase].join("; ");
-      }
-   }
+let Inhabitant = function(species, name, legs, hands, gender, phrase) {
+  this.species = species;
+  this.name = name;
+  this.legs = legs;
+  this.hands = hands;
+  this.gender = gender;
+  this.phrase = phrase;
 }
-const dog = Inhabitant('dog', 'Barsik', 4, 0, 'male', 'Woof woof', 'Alex', 'Bobby'),
-      man = Inhabitant('man', 'Max', 2, 2, 'male', 'Hello world!!'),
-      woman = Inhabitant('woman', 'Merry', 2, 2, 'female', 'Hello all'),
-      cat = Inhabitant('cat', 'Tom', 4, 0, 'male', 'Meow'),
-      catWoman = Inhabitant('cat-woman', 'Kitty', 2, 2, 'female', cat.phrase);
+
+let getInhabitantInfo = function(inhabitant) {
+  return [inhabitant.species, inhabitant.name, inhabitant.legs, inhabitant.hands, inhabitant.gender, inhabitant.phrase].join("; ")
+}
+
+const dog = new Inhabitant('dog', 'Barsik', 4, 0, 'male', 'Woof woof', 'Alex', 'Bobby'),
+      man = new Inhabitant('man', 'Max', 2, 2, 'male', 'Hello world!!'),
+      woman = new Inhabitant('woman', 'Merry', 2, 2, 'female', 'Hello all'),
+      cat = new Inhabitant('cat', 'Tom', 4, 0, 'male', 'Meow'),
+      catWoman = new Inhabitant('cat-woman', 'Kitty', 2, 2, 'female', cat.phrase);
 // ======== OUTPUT ========
-print(dog.getInfo());
-print(man.getInfo());
-print(woman.getInfo());
-print(cat.getInfo());
-print(catWoman.getInfo());
+print(getInhabitantInfo(dog));
+print(getInhabitantInfo(man));
+print(getInhabitantInfo(woman));
+print(getInhabitantInfo(cat));
+print(getInhabitantInfo(catWoman));
